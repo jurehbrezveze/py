1ca8ebd19ba6bf9f5181b75959751ccaaa290ad5
jurehbrezveze <73191906+jurehbrezveze@users.noreply.github.com>
2024-09-11 08:25:49 +0000

    ok

---
+++ casino.py
@@ -0,0 +1,80 @@
+import random
+
+def dice(i):
+    if(i==1):
+        print("┌───┐")
+        print("│O  │")
+        print("│   │")
+        print("│  O│")
+        print("└───┘")
+    if(i==2):
+        print("┌───┐")
+        print("│O  │")
+        print("│   │")
+        print("│  O│")
+        print("└───┘")
+    if(i==3):
+        print("┌───┐")
+        print("│O  │")
+        print("│ O │")
+        print("│  O│")
+        print("└───┘")
+    if(i==4):
+        print("┌───┐")
+        print("│O O│")
+        print("│   │")
+        print("│O O│")
+        print("└───┘")
+    if(i==5):
+        print("┌───┐")
+        print("│O O│")
+        print("│ O │")
+        print("│O O│")
+        print("└───┘")
+    if(i==6):
+        print("┌───┐")
+        print("│O O│")
+        print("│O O│")
+        print("│O O│")
+        print("└───┘")
+
+def kocke(coins):
+
+    coins = coins - 1
+
+    t = 0
+    j = int(input("Koliko bo seštevek meta dveh kock?\n"))
+
+    i = random.randint(1,6)
+    dice(i)
+    t = t + i
+
+    i = random.randint(1,6)
+    dice(i)
+    t = t + i
+
+    if(j==t):
+        print("Zmagal si, dobiš 10 kovancev")
+        coins = coins + 10
+    else:
+        print("Ni ti uspelo (womp womp)")
+
+    return(coins)
+def main():
+    print("░█████╗░░█████╗░░██████╗██╗███╗░░██╗░█████╗░")
+    print("██╔══██╗██╔══██╗██╔════╝██║████╗░██║██╔══██╗")
+    print("██║░░╚═╝███████║╚█████╗░██║██╔██╗██║██║░░██║")
+    print("██║░░██╗██╔══██║░╚═══██╗██║██║╚████║██║░░██║")
+    print("╚█████╔╝██║░░██║██████╔╝██║██║░╚███║╚█████╔╝")
+    print("░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝╚═╝░░╚══╝░╚════╝░\n\n\n\n\n")
+    coins = 10
+    while(True):
+        print(f"Imaš >{coins}< kovancev")
+        i = int(input("Katero igro želiš igrati?\n 1 - kockanje (1 kovanec)\n če si zadovoljen z svojimi dobički klikni ^C\n"))
+        if(i==1):
+            coins = kocke(coins)
+
+
+
+
+main()
